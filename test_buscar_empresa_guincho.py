02484bbb6de8606296893bb1aaf3109825888b0b
gabrielpassos2008@gmail <gabrielpassos2008@gmail>
2025-07-09 21:23:51 -0300

    criei a def test_autorizar_empresa_guincho

--- test_buscar_empresa_guincho.py
+++ test_buscar_empresa_guincho.py
@@ -11,16 +11,22 @@ def conexao():
     yield conn
     conn.close()
 
-@pytest.fixture
-def tabela_empresa_guincho(conexao):
+def test_buscar_empresa_guincho(conexao):
     model.adicionar_usuario_adm('1234','1234@gmail.com',1)
     model.adicionar_empresa_de_guincho(conexao,'Guincho Express', 'express@guincho.com', 'exp123')
     model.adicionar_empresa_de_guincho(conexao,'Resgate Rápido', 'rapido@guincho.com', 'rapido456')
     model.autorizar_empresa_guincho(conexao,1,1,1)
     model.autorizar_empresa_guincho(conexao,1,1,2)
-
-def test_buscar_empresa_guincho(conexao, tabela_empresa_guincho):
     usuarios = model.buscar_empresa_guincho(conexao)
     assert (1,'Guincho Express') in usuarios
     assert (2,'Resgate Rápido') in usuarios
     assert len(usuarios) == 2
+
+def test_autorizar_empresa_guincho(conexao):
+    model.adicionar_usuario_adm('1234','1234@gmail.com',1)
+    model.adicionar_empresa_de_guincho(conexao,'Guincho Express', 'express@guincho.com', 'exp123')
+    assert model.autorizar_empresa_guincho(conexao,1,1,1) == True
+    assert model.autorizar_empresa_guincho(conexao,1,1,4) == False
+
+
+
